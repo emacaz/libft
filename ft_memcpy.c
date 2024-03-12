4948b7ed91cba0fce59d0a5eb1d124e33e5cb35e
Emanuel Castillo Zambrano <emcastil@c2r3s1.42madrid.com>
2024-03-12 16:17:24 +0100

    ft_memcpy.c done

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,7 +12,20 @@
 
 #include <string.h>
 
-void *ft_memcpy(void *dst, const void *src, size_t n)
+// Copies 'n' bytes from 'src' to 'dst'
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
 
+	i = 0;
+	d = (unsigned char *)dst;
+	s = (const unsigned char *)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
 }
