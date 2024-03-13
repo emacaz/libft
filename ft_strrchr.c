823574b8deb1081866d3ae26e75095ec8d0bb603
Emanuel Castillo Zambrano <emcastil@c2r15s4.42madrid.com>
2024-03-13 15:40:56 +0100

    ft_strrchr.c done

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,32 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/13 13:24:35 by emcastil          #+#    #+#             */
-/*   Updated: 2024/03/13 13:24:37 by emcastil         ###   ########.fr       */
+/*   Created: 2024/03/13 14:59:48 by emcastil          #+#    #+#             */
+/*   Updated: 2024/03/13 14:59:50 by emcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-// Returns first concurrence found of 'c' in "s"
-char	*ft_strchr(const char *s, int c)
+// Returns last concurrence found of 'c' in "s"
+char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 
 	if (s == NULL)
 		return (NULL);
 	i = 0;
-	while (s[i] != '\0' || (s[i] == '\0' && c == '\0'))
+	while (s[i] != '\0')
 	{
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&s[i]);
+	while (i != 0)
+	{
+		i--;
 		if (s[i] == (char)c)
 		{
 			return ((char *)&s[i]);
 		}
-		i++;
 	}
 	return (NULL);
 }
