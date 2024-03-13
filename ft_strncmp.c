1a917a31dc20fc2637ae895c4fa778d17cf6485c
Emanuel Castillo Zambrano <emcastil@c2r15s4.42madrid.com>
2024-03-13 15:47:36 +0100

    I've copied 'ft_strncmp' from my piscine-projectss and I should review the whole code

---
+++ ft_strncmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 15:41:36 by emcastil          #+#    #+#             */
+/*   Updated: 2024/03/13 15:41:39 by emcastil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	while ((s1[i] != '\0' && s2[i] != '\0') && i < n - 1)
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
