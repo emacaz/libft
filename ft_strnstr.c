a1615313ea3f7886f21e2f765f64d61675367fb0
Emanuel Castillo Zambrano <emcastil@c2r3s4.42madrid.com>
2024-03-14 13:45:59 +0100

    ft_strnstr.c done

---
+++ ft_strnstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 13:18:49 by emcastil          #+#    #+#             */
+/*   Updated: 2024/03/14 13:18:54 by emcastil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (i < len)
+	{
+		j = 0;
+		while (needle[j] != '\0' && haystack[i + j] != '\0'
+			&& i + j < len && needle[j] == haystack[i + j])
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
