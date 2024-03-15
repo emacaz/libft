0b1e0f50d67f8ee003b750d1f1daf35d23506779
Emanuel Castillo Zambrano <emcastil@c2r9s3.42madrid.com>
2024-03-15 11:05:09 +0100

    ft_calloc.c done

--- ft_calloc.c
+++ ft_calloc.c
@@ -11,8 +11,20 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <errno.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	return (0);
+	size_t	total_size;
+	void	*ptr;
+
+	total_size = count * size;
+	if (size != 0 && total_size / size != count)
+		return (NULL);
+	ptr = malloc(total_size);
+	if (ptr != NULL)
+		ft_memset(ptr, 0, total_size);
+	else
+		errno = ENOMEM;
+	return (ptr);
 }
