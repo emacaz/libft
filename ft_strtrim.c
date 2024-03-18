d14be580ce63b150a15fdcea88e2fcba8616dfa5
Emanuel Castillo Zambrano <emcastil@c2r10s1.42madrid.com>
2024-03-18 20:52:04 +0100

    working on ft_split.c - ft_strtrim.c is having problem with compiler

---
+++ ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 20:01:15 by emcastil          #+#    #+#             */
+/*   Updated: 2024/03/18 20:01:17 by emcastil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (ft_strrchr(set, s1[len]) && len)
+		len--;
+	return (ft_substr(s1, 0, len + 1));
+}
