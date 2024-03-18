7bb603cf0093c77e9062aa78144b7bdc58aed40b
Emanuel Castillo Zambrano <emcastil@c2r10s1.42madrid.com>
2024-03-18 21:48:02 +0100

    ft_strtrim.c done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,16 +12,41 @@
 
 #include "libft.h"
 
+// Check if char 'c' is in 'set'
+static int	ft_is_in_set(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+// Creates a new string by removing characters in 'set' from both ends of 's1'
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len;
+	char	*str;
+	size_t	i;
+	size_t	start;
+	size_t	end;
 
-	if (!s1 || !set)
-		return (0);
-	while (*s1 && ft_strchr(set, *s1))
-		s1++;
-	len = ft_strlen(s1);
-	while (ft_strrchr(set, s1[len]) && len)
-		len--;
-	return (ft_substr(s1, 0, len + 1));
+	start = 0;
+	while (s1[start] && ft_is_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_is_in_set(s1[end - 1], set))
+		end--;
+	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = 0;
+	return (str);
 }
