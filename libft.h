9539dc025863ef4fda572a07f374aa9bdb738d49
Emanuel Castillo Zambrano <emcastil@c2r9s5.42madrid.com>
2024-03-19 20:34:03 +0100

    ft_putchar_fd.c done

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 # include <stddef.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_list
 {
@@ -52,5 +53,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
 
 #endif
