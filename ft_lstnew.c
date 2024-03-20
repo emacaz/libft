169ac521266252fcf2a19cb6272ac1ba2c04bb68
Emanuel Castillo Zambrano <emcastil@c2r14s2.42madrid.com>
2024-03-20 15:22:39 +0100

    ft_lstnew.c done

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -14,12 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*new_node;
+	t_list	*node;
 
-	new_node = malloc(sizeof(t_list));
-	if (new_node == NULL)
+	node = malloc(sizeof(t_list));
+	if (node == NULL)
 		return (0);
-	new_node -> content = content;
-	new_node -> next = NULL;
-	return (new_node);
+	node -> content = content;
+	node -> next = NULL;
+	return (node);
 }
