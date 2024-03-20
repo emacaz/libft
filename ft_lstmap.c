b88d45ce2089be9f49cac3747b0dca6263ee5639
Emanuel Castillo Zambrano <emcastil@c2r14s2.42madrid.com>
2024-03-20 17:33:35 +0100

    ft_lstmap.c done

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,5 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	return (NULL);
+	t_list	*element;
+	t_list	*new;
+	void	*content;
+
+	new = NULL;
+	while (lst)
+	{
+		content = f(lst -> content);
+		element = ft_lstnew(content);
+		if (!element)
+		{
+			free(content);
+			ft_lstclear(&new, del);
+			return (0);
+		}
+		ft_lstadd_back(&new, element);
+		lst = lst -> next;
+	}
+	return (new);
 }
