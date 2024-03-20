498f8b64235ba79feef76015ae990116a1923fab
Emanuel Castillo Zambrano <emcastil@c2r14s2.42madrid.com>
2024-03-20 15:35:21 +0100

    ft_lstadd_front.c done

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/20 13:37:28 by emcastil          #+#    #+#             */
-/*   Updated: 2024/03/20 13:37:53 by emcastil         ###   ########.fr       */
+/*   Created: 2024/03/20 15:23:30 by emcastil          #+#    #+#             */
+/*   Updated: 2024/03/20 15:26:24 by emcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Create new node with 'content'
-t_list	*ft_lstnew(void *content)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*node;
-
-	node = malloc(sizeof(t_list));
-	if (node == NULL)
-		return (0);
-	node -> content = content;
-	node -> next = NULL;
-	return (node);
+	if (new != NULL)
+	{
+		new -> next = *lst;
+		*lst = new;
+	}
 }
